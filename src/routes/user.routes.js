af36812b7b097c19ae2ff6e07f66077b8046de98
unknown <64825962+D1urga@users.noreply.github.com>
2024-03-09 13:05:41 +0530

    first code

---
+++ src/routes/user.routes.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { registerUser } from "../controllers/user.controllers.js";
+
+const router = Router();
+
+router.route("/register").post(registerUser);
+
+export default router;
